27f8e05e4160cfed3e210bf1ba960c99d61b444a
iam111 <64158683+iam111@users.noreply.github.com>
2020-12-14 21:51:36 -0800

    Add files via upload

---
+++ routes/saloon_peoples.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+const mysql = require('mysql')
+
+const conn = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "db"
+});
+
+
+/* GET home page. */
+router.get('/:name', function (req, res, next) {
+    var sql = 'SELECT * FROM test WHERE saloon_name =' + mysql.escape(req.params.name);
+    conn.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log(result);
+        res.send(JSON.stringify({ "result": result }))
+        //res.send("<h1>Hello</h1> " + JSON.stringify(result[0]) )
+    })
+   // res.send(req.params.name);
+});
+
+module.exports = router;
